fb21f96bae45aff51db34d19239b609fb8a9310b
pedroigor <pigor.craveiro@gmail.com>
2012-09-05 02:27:42 -0300

    Undo Initial code for the JPA Identity Store/Management

--- impl/src/main/java/org/jboss/picketlink/idm/internal/JPAIdentityStore.java
+++ impl/src/main/java/org/jboss/picketlink/idm/internal/JPAIdentityStore.java
@@ -3,19 +3,7 @@ package org.jboss.picketlink.idm.internal;
 import java.util.List;
 import java.util.Map;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
-
-import org.jboss.picketlink.idm.internal.jpa.DatabaseGroup;
-import org.jboss.picketlink.idm.internal.jpa.DatabaseMembership;
-import org.jboss.picketlink.idm.internal.jpa.DatabaseRole;
-import org.jboss.picketlink.idm.internal.jpa.DatabaseUser;
-import org.jboss.picketlink.idm.internal.jpa.JPACallback;
-import org.jboss.picketlink.idm.internal.jpa.JPATemplate;
-import org.jboss.picketlink.idm.internal.jpa.NamedQueries;
 import org.jboss.picketlink.idm.model.Group;
-import org.jboss.picketlink.idm.model.IdentityType;
 import org.jboss.picketlink.idm.model.Membership;
 import org.jboss.picketlink.idm.model.Role;
 import org.jboss.picketlink.idm.model.User;
@@ -30,127 +18,92 @@ import org.jboss.picketlink.idm.spi.IdentityStore;
  * An implementation of IdentityStore backed by a JPA datasource 
  *
  * @author Shane Bryzak
- * @author <a href="mailto:psilva@redhat.com">Pedro Silva</a>
  */
 public class JPAIdentityStore implements IdentityStore
 {
 
-    private JPATemplate jpaTemplate;
-
     @Override
     public User createUser(String name)
     {
-        final DatabaseUser newUser = new DatabaseUser(name);
-        
-        persist(newUser);
-        
-        return newUser;
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
-    public void removeUser(final User user)
+    public void removeUser(User user)
     {
-        remove(user);
+        // TODO Auto-generated method stub
+        
     }
 
     @Override
-    public User getUser(final String name)
+    public User getUser(String name)
     {
-        final String namedQueryName = NamedQueries.USER_LOAD_BY_KEY;
-        
-        return (User) findIdentityTypeByKey(name, namedQueryName);
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public Group createGroup(String name, Group parent)
     {
-        DatabaseGroup newGroup = new DatabaseGroup(name);
-        
-        newGroup.setParentGroup((DatabaseGroup) parent);
-
-        persist(newGroup);
-        
-        return newGroup;
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public void removeGroup(Group group)
     {
-        remove(group);
+        // TODO Auto-generated method stub
+        
     }
 
     @Override
-    public Group getGroup(String group)
+    public Group getGroup(String name)
     {
-        return (Group) findIdentityTypeByKey(group, NamedQueries.GROUP_LOAD_BY_KEY);
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public Role createRole(String name)
     {
-        DatabaseRole newRole = new DatabaseRole(name);
-        
-        persist(newRole);
-        
-        return newRole;
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public void removeRole(Role role)
     {
-        remove(role);
+        // TODO Auto-generated method stub
+        
     }
 
     @Override
     public Role getRole(String role)
     {
-        return (Role) findIdentityTypeByKey(role, NamedQueries.ROLE_LOAD_BY_KEY);
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public Membership createMembership(Role role, User user, Group group)
     {
-        DatabaseMembership newMembership = new DatabaseMembership(role,user,group);
-        
-        persist(newMembership);
-        
-        return newMembership;
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
     public void removeMembership(Role role, User user, Group group)
     {
-        Membership membership = getMembership(role, user, group);
+        // TODO Auto-generated method stub
         
-        if (membership != null) {
-            remove(membership);
-        }
     }
 
     @Override
-    public Membership getMembership(final Role role, final User user, final Group group)
+    public Membership getMembership(Role role, User user, Group group)
     {
-        return (Membership) executeOperation(new JPACallback() {
-            
-            @Override
-            public Object execute(EntityManager entityManager) {
-                Query query = entityManager.createNamedQuery(NamedQueries.MEMBERSHIP_LOAD_BY_KEY);
-                
-                query.setParameter("role", role);
-                query.setParameter("user", user);
-                query.setParameter("group", group);
-                
-                Membership loadedMembership = null;
-                
-                try {
-                    loadedMembership = (Membership) query.getSingleResult();
-                } catch (NoResultException nre) {
-                    // TODO: what to do when this happens 
-                }
-
-                return loadedMembership;
-            }
-        });
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
@@ -265,80 +218,4 @@ public class JPAIdentityStore implements IdentityStore
         return null;
     }
 
-    public void setJpaTemplate(JPATemplate jpaTemplate) {
-        this.jpaTemplate = jpaTemplate;
-    }
-    
-    /**
-     * <p>Executes the {@link JPACallback} instance.</p>
-     * 
-     * @param callback
-     * @return
-     */
-    private Object executeOperation(JPACallback callback) {
-        return this.jpaTemplate.execute(callback);
-    }
-
-    /**
-     * <p>Persists a specific instance.</p>
-     * 
-     * @param entity
-     */
-    private void persist(final Object entity) {
-        JPACallback callback = new JPACallback() {
-            
-            @Override
-            public Object execute(EntityManager entityManager) {
-                entityManager.persist(entity);
-                return null;
-            }
-        };
-        
-        executeOperation(callback);
-    }
-    
-    /**
-     * <p>Removes a specific instance.</p>
-     * 
-     * @param entity
-     */
-    private void remove(final Object entity) {
-        executeOperation(new JPACallback() {
-            
-            @Override
-            public Object execute(EntityManager entityManager) {
-                entityManager.remove(entity);
-                return null;
-            }
-        });
-    }
-    
-    /**
-     * <p>Find a instance with the given name and using the specified named query.</p>
-     * 
-     * @param name
-     * @param namedQueryName
-     * @return
-     */
-    private IdentityType findIdentityTypeByKey(final String name, final String namedQueryName) {
-        return (IdentityType) executeOperation(new JPACallback() {
-            
-            @Override
-            public Object execute(EntityManager entityManager) {
-                Query query = entityManager.createNamedQuery(namedQueryName);
-                
-                query.setParameter("key", name);
-
-                Object loadedUser = null;
-                
-                try {
-                    loadedUser = query.getSingleResult();
-                } catch (NoResultException nre) {
-                    // TODO: what to do when this happens 
-                }
-
-                return loadedUser;
-            }
-        });
-    }
 }
